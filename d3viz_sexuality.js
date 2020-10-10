30c4ce19e7f1f5b29719c1a22cafbcfee8576820
André Fincato <work@andrefincato.info>
2020-10-10 15:40:11 +0200

    adding comments / making sense of what’s going on

--- d3viz_sexuality.js
+++ d3viz_sexuality.js
@@ -11,81 +11,90 @@ let svg;
 d3.json('data/dataset_selection.json')
   .then(function(data) {
 
-    x.domain(data.map((d)=> {
-      if (d.description!=""){
-      return d.description;
-    }}))
-    .range([200, width-50]);
+    // set x-axis
+    x.domain(data.map((d) => {
+      if (d.description != ""){
+        return d.description;
+      }}))
+      .range([200, width - 50]);
 
-    y.domain(data.map((d)=> {
+    // set y-axis
+    y.domain(data.map((d) => {
       return d.publisher;
     }))
-    .range([50, height - 50]);
+      .range([50, height - 50]);
 
-    color.domain(data.map((d)=> {
-      if (d.description_second!="")
+    color.domain(data.map((d) => {
+      if (d.description_second != "")
       return d.description_second;
     }));
 
     console.log(y.domain());
 
     let simulation = d3.forceSimulation(data)
-      .force('x', d3.forceX( (d) => {
-        if (d.description!=""){
-        return x(d.description)
-      }}).strength(0.99))
-      .force('y', d3.forceY( (d) => {
-        return y(d.publisher);
-      } ).strength(0.99))
-      .force('collide', d3.forceCollide(5).iterations(1))
-      .alphaDecay(0)
-      .alpha(0.1)
-      .on('tick', tick)
+        .force('x', d3.forceX((d) => {
+          if (d.description != ""){
+            return x(d.description)
+          }}).strength(0.99))
+        .force('y', d3.forceY((d) => {
+          return y(d.publisher);
+        } ).strength(0.99))
+        .force('collide', d3.forceCollide(5).iterations(1))
+        .alphaDecay(0)
+        .alpha(0.1)
+        .on('tick', tick)
 
-      let init_decay;
-      init_decay = setTimeout(function(){
-        console.log('init alpha decay')
-        simulation.alphaDecay(0.1);
-      }, 5000);
+    let init_decay;
+    init_decay = setTimeout(function(){
+      console.log('init alpha decay')
+      simulation.alphaDecay(0.1);
+    }, 5000);
 
-    svg = d3.selectAll("#fst").append("svg")
-    .attr("width", width)
-    .attr("height", height);
+    svg = d3.select("#fst").append("svg")
+      .attr("width", width)
+      .attr("height", height);
 
-    let item = svg.append("g");
+    let item = svg.append("g").attr("class", "canvas");
 
-    item.selectAll("rect")
-    .data(data)
-    .enter()
-    .append("rect")
-    .attr("width", 8)
-    .attr("height", 10)
-    .attr("x", width / 2)
-    .attr("y", height / 2)
-    .attr("fill", (d) => {
-      return color(d.th)}
-    )
+    // create rectangles with data available
+    item.selectAll(".canvas")
+      .data(data)
+      .enter()
+      .append("rect")
+      .attr("width", 8)
+      .attr("height", 10)
+      .attr("x", width / 2)
+      .attr("y", height / 2)
+      .attr("fill", (d) => {
+        return color(d.th)
+      })
     .attr("stroke", "rgba(0,0,0,.2)");
 
     d3.selectAll("rect").on("mouseover", (d) => {
       console.log(d.description_second + "\n" + d.title + "\n" + d.author)
     })
+      .on("mousemove", () => {
+        return tooltip
+          .style("top", (d3.event.pageY-10)+"px")
+          .style("left",(d3.event.pageX+10)+"px");
+      })
+      .on("mouseout", () => {
+        return tooltip.style("visibility", "hidden");
+      });
 
-    function tick(){
+    function tick () {
+      d3.selectAll('rect')
+        .attr('x', function (d) { return d.x })
+        .attr('y', function (d) { return d.y })
+    };
 
-        d3.selectAll('rect')
-        .attr('x', function(d){return d.x})
-        .attr('y', function(d){return d.y})
+    svg.append("g")
+      .attr("transform", "translate(0," + 0 + ")")
+      .call(d3.axisBottom(x));
 
-      };
-
-      svg.append("g")
-        .attr("transform", "translate(0," + 0 + ")")
-        .call(d3.axisBottom(x));
-
-      svg.append("g")
-        .attr("transform", "translate(0," + 0 + ")")
-        .call(d3.axisRight(y));
+    svg.append("g")
+      .attr("transform", "translate(0," + 0 + ")")
+      .call(d3.axisRight(y));
   })
   .catch(function(error){
 
